6210124843ba486677fbf8588200eb81060a851e
Oscar OZSERTTAS <oozsertt@e1r11p2.42.fr>
2021-03-17 10:37:50 +0100

    every library for main removed

--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,15 @@
 /*   By: oozsertt <oozsertt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 10:01:28 by oozsertt          #+#    #+#             */
-/*   Updated: 2021/03/12 10:24:42 by oozsertt         ###   ########.fr       */
+/*   Updated: 2021/03/17 10:36:50 by oozsertt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define DEBUG printf("ICI\n");
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdio.h>
 
 int		get_next_line(int fd, char **line);
 char	*ft_strchr_gnl(const char *s, int c);
